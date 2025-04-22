a2a7508ecfa1d7a27ad7288e374a08a9ebd20912
Jeronimo Ferrer Bail <108182648+JeroFerrer93@users.noreply.github.com>
2025-04-22 16:45:48 -0300

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,24 +1,37 @@
-import random
+pip install random-word
 
-conjunto_palabras = ['messi', 'scaloneta', 'cuti', 'dybala', 'garnacho']
-palabra = random.choice(conjunto_palabras)
+from random_word import RandomWords
+
+r = RandomWords()
+palabra = r.get_random_word()
+
+# Validamos que la palabra sea válida (sin símbolos raros, espacios, etc.)
+while not palabra.isalpha() or len(palabra) < 3 or len(palabra) > 10:
+    palabra = r.get_random_word()
+
+palabra = palabra.lower()
 adivina_palabra = ['_'] * len(palabra)
 
 print("Bienvenidos al juego de adivinar las palabras correctas!")
+
 attempts = 10
+
 while attempts > 0:
-    print('\nCurrent word: ' + ' '.join(guessedWord))
-    guess = input('Guess a letter: ').lower()
-    if guess in word:
-        for i in range(len(word)):
-            if word[i] == guess:
-                guessedWord[i] = guess
-        print('Great guess!')
+    print('\nPalabra actual: ' + ' '.join(adivina_palabra))
+    guess = input('🔤 Adiviná una letra: ').lower()
+
+    if guess in palabra:
+        for i in range(len(palabra)):
+            if palabra[i] == guess:
+                adivina_palabra[i] = guess
+        print('✅ ¡Buena letra!')
     else:
         attempts -= 1
-        print('Wrong guess! Attempts left: ' + str(attempts))
-    if '_' not in guessedWord:
-        print('\nCongratulations!! You guessed the word: ' + word)
+        print(f'❌ Letra incorrecta. Intentos restantes: {attempts}')
+
+    if '_' not in adivina_palabra:
+        print('\n🎉 ¡Felicitaciones! Adivinaste la palabra: ' + palabra)
         break
-    else:
-      print('\nYou\'ve run out of attempts! The word was: ' + word)
+
+if '_' in adivina_palabra:
+    print('\n😢 ¡Te quedaste sin intentos! La palabra era: ' + palabra)
